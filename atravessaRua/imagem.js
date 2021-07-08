9f71e7db17a819989559231bf45be412697e6acf
Patrick <66400161+patrickfreiree@users.noreply.github.com>
2021-07-08 14:06:57 -0300

    Create imagem.js

---
+++ atravessaRua/imagem.js
@@ -0,0 +1,39 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+//let imagemCarro4;
+//let imagemCarro5;
+let imagemCarro6;
+let imagemMoto;
+
+function preload() {
+  imagemDaEstrada = loadImage("imagem/estrada.png");
+  imagemDoAtor = loadImage("imagem/zeeus.png");
+  imagemCarro = loadImage("imagem/carro-1.png");
+  imagemCarro2 = loadImage("imagem/carro-2.png");
+  imagemCarro3 = loadImage("imagem/carro-3.png");
+  imagemCarro4 = loadImage("imagem/carro-1.png");
+  imagemCarro5 = loadImage("imagem/carro-1.png");
+  imagemCarro6 = loadImage("imagem/carro-2.png");
+  imagemMoto = loadImage("imagem/motoo.png");
+  imagemMoto2 = loadImage("imagem/motoo.png");
+ // imagemMoto3 = loadImage("imagem/motoo.png");
+
+  imagemCarros = [
+    imagemCarro,
+    imagemCarro2,
+    imagemCarro3,
+    //imagemCarro4,
+    //imagemCarro5,
+    imagemCarro6,
+  ];
+  
+  imagemMotos = [
+   imagemMoto,
+   imagemMoto2,
+   // imagemMoto3
+  ];
+  
+}
