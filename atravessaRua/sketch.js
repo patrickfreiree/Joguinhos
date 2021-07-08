44533072e463b2dd82d195f210896e2a546b8f22
Patrick <66400161+patrickfreiree@users.noreply.github.com>
2021-07-08 14:05:12 -0300

    Create sketch.js

---
+++ atravessaRua/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  posicaoInicial();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+  
+}
