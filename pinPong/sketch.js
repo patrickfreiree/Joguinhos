483000f38a175750320ee2d00e142c525c5ebcdb
Patrick <66400161+patrickfreiree@users.noreply.github.com>
2021-07-15 14:03:35 -0300

    Update sketch.js

--- pinPong/sketch.js
+++ pinPong/sketch.js
@@ -1,6 +1,7 @@
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 20;
+let raio = diametro/2; 
 
 let velocidadeBolinhaX = 3;
 let velocidadeBolinhaY = 3;
@@ -15,10 +16,10 @@ function draw() {
   xBolinha += velocidadeBolinhaX;
   yBolinha += velocidadeBolinhaY;
   
-  if (xBolinha > width || xBolinha < 0) {
+  if (xBolinha + raio > width || xBolinha < 0) {
     velocidadeBolinhaX *= -1;
   }
-  if (yBolinha > height || yBolinha < 0){
+  if (yBolinha + raio > height || yBolinha < 0){
     velocidadeBolinhaY *= -1
   } 
 }
